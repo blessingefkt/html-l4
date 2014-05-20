c094b5bfcf48c87411b1f84e5ad9d6ebe237804b
Blessing Osakue <iyoworks@gmail.com>
2014-05-20 10:02:17 -0500

    updated table class to accept items and headers. removed builder redundancy.

--- src/Html/Tables/Table.php
+++ src/Html/Tables/Table.php
@@ -1,51 +1,85 @@
 <?php namespace Iyoworks\Html\Tables;
 
+use Iyoworks\Support\Str;
+
 /**
  * Class Table
  * @package Iyoworks\Html\Tables
  */
 class Table extends BaseTable {
 
-    /**
-     * @var callable
-     */
-    protected $builder;
+	/**
+	 * @var callable
+	 */
+	protected $builder;
+	/**
+	 * @var array|\ArrayAccess|\ArrayIterator
+	 */
+	public $items;
 
-    /**
-     * @param string $label
-     * @param array $attributes
-     */
-    function __construct($label = null, array $attributes = [])
-    {
-        parent::__construct($label, $attributes);
-    }
+	/**
+	 * @param string $label
+	 * @param array $attributes
+	 */
+	function __construct($label = null, array $attributes = [])
+	{
+		parent::__construct($label, $attributes);
+	}
 
-    /**
-     * @param null $label
-     * @param array $attributes
-     * @return \Iyoworks\Html\Tables\Table
-     */
-    public static function make($label = null, array $attributes = [])
-    {
-        return new static($label, $attributes);
-    }
+	/**
+	 * @param null $label
+	 * @param array $attributes
+	 * @return \Iyoworks\Html\Tables\Table
+	 */
+	public static function make($label = null, array $attributes = [])
+	{
+		return new static($label, $attributes);
+	}
 
+	/**
+	 * @return mixed
+	 */
+	protected function buildTable()
+	{
+		return call_user_func($this->builder, $this);
+	}
 
-    /**
-     * @param callable $builder
-     */
-    public function setBuilder(callable $builder)
-    {
-        $this->builder = $builder;
-    }
+	/**
+	 * @param array $items
+	 * @return $this
+	 */
+	public function setItems($items)
+	{
+		$this->items = $items;
+		return $this;
+	}
 
-    /**
-     * @return mixed
-     */
-    protected function buildTable()
-    {
-        return call_user_func($this->builder, $this);
-    }
+	/**
+	 * @param array $headers
+	 * @return $this
+	 */
+	public function setHeaders(array $headers)
+	{
+		foreach ($headers as $k => $header)
+		{
+			$this->header(is_int($k) ? Str::slug($header, '_') : $k, $header);
+		}
+		return $this;
+	}
 
-
+	/**
+	 * @param callable $build
+	 * @return $this
+	 */
+	public function builder(callable $build)
+	{
+		$this->builder = function ($table) use ($build)
+		{
+			foreach ($table->items as $key => $item)
+			{
+				call_user_func($build, $table, $item, $key);
+			}
+		};
+		return $this;
+	}
 } 
